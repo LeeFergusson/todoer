bb2d15c62adba21ea8359e705ca33a8be80920ca
Lee Fergusson <lee.fergusson@googlemail.com>
2022-09-15 19:25:40 +0100

    Create basic header structure

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
-    <div className="p-2">
-      <h1>ToDo Application</h1>
+    <div className="flex p-2 border-b-2">
+      <h1 className="text-2xl">
+        <Link href="/">ToDo</Link>
+      </h1>
+      <div className="flex justify-between w-full">
+        <ul className="my-auto">
+          <li className="mx-2 align-middle">User Nav</li>
+        </ul>
+        <ul className="my-auto">
+          <li>Session Nav</li>
+        </ul>
+      </div>
     </div>
   );
 };
