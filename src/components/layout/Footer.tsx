64eb40f185cbbacdbb86fa587d6fa3b85a8bc31d
Lee Fergusson <lee.fergusson@googlemail.com>
2022-09-16 01:42:41 +0100

    Improve page footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,12 +1,21 @@
 const Footer = () => {
   return (
     <div className="flex justify-between p-2 border-t-2">
-      <small>ToDoer</small>
+      <small>
+        <a
+          className="hover:underline hover:underline-offset-2"
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/LeeFergusson/todoer"
+        >
+          ToDoer@github
+        </a>
+      </small>
       <small>A next js Application</small>
       <small>
         Created by{" "}
         <a
-          className="underline underline-offset-2"
+          className="hover:underline hover:underline-offset-2"
           target="_blank"
           rel="noreferrer"
           href="https://github.com/LeeFergusson"
