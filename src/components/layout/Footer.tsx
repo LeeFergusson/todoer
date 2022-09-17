19a059de6a955b9ee1cd5e09845af92394250b4b
Lee Fergusson <lee.fergusson@googlemail.com>
2022-09-17 17:43:19 +0100

    Update footer for authenticated users

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,16 +1,25 @@
+import { useSession } from "next-auth/react";
+
 const Footer = () => {
+  const session = useSession();
+
   return (
     <div className="border-t-2">
       <div className="container mx-auto flex justify-between p-2">
-        <small>
-          <a
-            className="hover:underline hover:underline-offset-2"
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/LeeFergusson/todoer">
-            ToDoer@github
-          </a>
-        </small>
+        {session.status === "authenticated" ? (
+          <small>Signed in as: {session.data.user?.name}</small>
+        ) : (
+          <small>
+            <a
+              className="hover:underline hover:underline-offset-2"
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/LeeFergusson/todoer">
+              ToDoer@github
+            </a>
+          </small>
+        )}
+
         <small>A next js Application</small>
         <small>
           Created by{" "}
