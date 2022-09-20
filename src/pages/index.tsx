f91a19b7650b7d86cf478edb8182a08ab9be3486
Lee Fergusson <lee.fergusson@googlemail.com>
2022-09-20 13:44:13 +0100

    Add basic list of lists

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from "next";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 
 const Home: NextPage = () => {
+  const session = useSession();
+
   return (
     <>
       <Head>
@@ -10,7 +13,23 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="container mx-auto flex flex-col flex-grow p-2">
-        <h2>Home Page</h2>
+        {session.status !== "authenticated" ? (
+          <h2>Home Page</h2>
+        ) : (
+          <section className="max-w-md border-2 p-2 rounded-md">
+            <div className="flex">
+              <h3 className="text-xl flex-grow">Recent Lists</h3>
+              <button className="bg-green-600 text-white p-2  rounded-lg text-sm mb-2 shadow-md">
+                Create
+              </button>
+            </div>
+            <hr />
+            <ul>
+              <li>Item 1</li>
+              <li>Item 1</li>
+            </ul>
+          </section>
+        )}
       </main>
     </>
   );
